b93f5d69e1d1d71c6107e2da8d0a4f924e835991
ppthaw <ppthaw07@gmail.com>
2022-02-17 19:18:41 +0630

    assignment3.cpp

--- assign1/z2p7cpp/assignment3.cpp
+++ assign1/z2p7cpp/assignment3.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std ;
 int main() {
-    int loan,rate,month,interest ;
+    int loan,rate,month,monthlyInterest,interest ;
     cout<<"Enter Loan Amount : " ;
     cin>>loan ;
     cout<<"Enter Rate :" ;	
     cin>>rate ;
     cout<<"Enter Number Of Month :" ;	
     cin>>month ;
-    loan = loan ;
-    rate = (loan * 10) / 100 ;
-    interest = rate * month ;
-    cout<< "Interest Value : "<<interest ;
+    monthlyInterest = loan * rate * 0.01 ;
+    interest = monthlyInterest * month ;
+    cout<< "Monthly Interest :"<<monthlyInterest<<endl;
+    cout<< "Your Interest is : "<<interest ;
     return 0 ;
 }
