5933fdd5c40303a43e648d2660f9624e92b534a1
ppthaw <ppthaw07@gmail.com>
2022-02-21 20:37:51 +0630

    loopassignment-1.cpp

--- assign1/z2p7cpp/loopassignment-1.cpp
+++ assign1/z2p7cpp/loopassignment-1.cpp
@@ -1,12 +1,16 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int pos=0,neg=0,zero=0,i ; 
-    cout<< " How many numbers you want to type : " ;
-    for(i=0,i<10,i++){
-        pos++
+#include<iostream>
+using namespace std ;
+int main(){
+    int zero=0,post=0,neg=0,num,count=1 ;
+    while(count<=10){
+        cout<< " Enter any number : " ;
+        cin>> num ;  
+        if( num==0 ) zero++ ;
+        if( num<0 ) neg++ ;-69
+        if( num>0 ) post++ ;
+        count++;
     }
-
-        
-    return 0;
+        cout<< " Numbers of zero : "<< zero << " \n Numbers of positive number : " << post << " \n Numbers of negative number : " << neg << endl ;
+    
+    return 0 ;
 }
