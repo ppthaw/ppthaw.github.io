b36db0d41d5e392905dd263dd799b7648239679d
ppthaw <ppthaw07@gmail.com>
2022-02-21 17:42:31 +0630

    while.cpp

--- assign1/z2p7cpp/condassignment-3.cpp
+++ assign1/z2p7cpp/condassignment-3.cpp
@@ -8,8 +8,10 @@ int main() {
     cin>>uPass ;
     if( uName=="mmit" && uPass=="admin123" ){
         cout<< " Login Success " ;
-    }else if( uName=="mmit"){
+    }else if( uName=="mmit" ){
         cout<< " Password is Incorrect " ;
+    }else if( uPass=="admin123" ){
+        cout<< " User Name is Incorrect " ;
     }else{
         cout<< " User Name and Password do not match " ;
     }
