76e0e2944c7fae2a1c02a085fe2b250d3992a8d0
ppthaw <ppthaw07@gmail.com>
2022-03-01 19:58:05 +0630

    Shoe.cpp

--- assign1/z2p7cpp/Product.cpp
+++ assign1/z2p7cpp/Product.cpp
@@ -8,14 +8,31 @@ private:
     string Name ;
     float Price ;
 public:
+    Product(){
+        barCode="000" ;
+        Name="Unknown" ;
+        Price=0.0 ;
+    }
+    Product(string barCode,string Name,float Price){
+        this->barCode=barCode ;
+        this->Name=Name ;
+        this->Price=Price;
+    }
     void showData(){
+        cout<< barCode << "\t" << Name << "\t" << Price << endl ;
     }
 };
 int main(){
-    Product product1 ;
-    product1.barCode = "0010" ; 
-   // product1.Name= "Cake" ;
-  //  product1.Price = 2300 ; 
+    string code ;
+    string name ;
+    float price ;
+    cout<< " Enter Product Code: " ;
+    cin>>code ;
+     cout<< " Enter Product Name: " ;
+    cin>>name ;
+    cout<< " Enter Price: " ;
+    cin>>price ;
+    Product product1(code,name,price) ;
     product1.showData() ;
-    return 0;
+    return 0 ;
 }
