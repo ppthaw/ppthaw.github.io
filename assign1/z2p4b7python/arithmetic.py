5703cac7f01c1f639f1c80eecc9e6323a7556187
ppthaw <ppthaw07@gmail.com>
2022-03-10 19:50:27 +0630

    arithmetic.py

---
+++ assign1/z2p4b7python/arithmetic.py
@@ -0,0 +1,19 @@
+# + , - , * , / , % , // , **
+num1=25
+num2=4
+sum=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+mod=num1%num2
+expo=num1**num2
+fdiv=num1//num2
+print("num1:", num1)
+print("num2:", num2)
+print("SUM:", sum)
+print("SUB:", sub)
+print("MUL:", mul)
+print("DIV", div)
+print("MOD", mod)
+print("EXPO", expo)
+print("FDIV", fdiv)
