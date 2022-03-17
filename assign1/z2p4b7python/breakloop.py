ef8505c1ca3accf9edf50fa9ec32d1803a57d208
ppthaw <ppthaw07@gmail.com>
2022-03-17 19:03:11 +0630

    sumcount.py

---
+++ assign1/z2p4b7python/breakloop.py
@@ -0,0 +1,24 @@
+
+
+for letter in 'Python':
+    if letter == 'h':
+        #break
+        continue #  one time skipped
+    print("Current Letter is :",letter)
+
+#var=10
+#while var>0:
+    #print("Var is :",var)
+    #var = var-1
+    #if var == 5:
+        #break
+#print('Bye!')
+
+var=10
+while var>0:    
+    var = var-1
+    if var == 5:
+        continue
+    print("Var is :",var)
+print('Bye!')
+
