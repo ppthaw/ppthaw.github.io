a0f97c160fa7f267345ade54a50f507f1a02f777
ppthaw <ppthaw07@gmail.com>
2022-03-24 19:29:14 +0630

    tempdict.py

--- assign1/z2p4b7python/tempdict.py
+++ assign1/z2p4b7python/tempdict.py
@@ -23,3 +23,18 @@ temps['Osolo']=15
 print(temps)
 del temps['Yangon']
 print(temps)
+if 'London' in temps:
+    print('London temperature is %s' %temps['London'])
+else:
+    print('London is not in temps!')
+
+# pass by reference
+t1=temps
+t1['Mandalay']=36
+print(temps)
+print(t1)
+
+t2=temps.copy() # pass by value
+t2['Mogok']=24
+print(t2)
+print(temps)
